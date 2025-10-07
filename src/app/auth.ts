16864ba68fa6431d8ede7c4ec59e60dcd9fb594a
hussammuhamed1 <20230122@stud.fci-cu.edu.eg>
2025-10-07 14:28:46 +0300

    change auth

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -18,56 +18,59 @@ export const authOption: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       authorize: async (credentials) => {
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_BASE_URL}api/v1/auth/signin`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              email: credentials?.email,
-              password: credentials?.password,
-            }),
+        try {
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_API_BASE_URL}api/v1/auth/signin`,
+            {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                email: credentials?.email,
+                password: credentials?.password,
+              }),
+            }
+          );
+
+          const data = await res.json();
+          console.log("API response:", data);
+
+          if (!res.ok) {
+            throw new Error(data?.message || "Invalid response from API");
           }
-        );
 
-        const data = await res.json();
-        console.log("API response:", data);
+          if (data?.token && data?.user) {
+            return { id: data.user._id, user: data.user, token: data.token };
+          }
 
-        if (res.ok && data?.token && data?.user) {
-        
-
-          return {
-            id: "55" , user: data.user, token: data.token
-          };
+          throw new Error("Login failed");
+        } catch (err) {
+          console.error("Authorize error:", err);
+          throw err;
         }
-
-        throw new Error(data?.message || "Login failed");
       },
     }),
   ],
-callbacks: {
-  async jwt({ token, user }) {
-    if (user) {
-      token.user = {
-        role: user.role,
-        name: user.name,
-        email: user.email,
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = {
+          role: user.role,
+          name: user.name,
+          email: user.email,
+        };
+        token.token = user.token ?? undefined;
       }
-      token.token = user.token ?? undefined
-    }
-    return token
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (token.user) {
+        session.user = token.user;
+      }
+      if (token.token) {
+        session.token = token.token;
+      }
+      return session;
+    },
   },
-
-  async session({ session, token }) {
-    if (token.user) {
-      session.user = token.user
-    }
-    if (token.token) {
-      session.token = token.token
-    }
-    return session
-  },
-}
-
-
 };
